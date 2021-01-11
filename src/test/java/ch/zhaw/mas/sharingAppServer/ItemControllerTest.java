6e37b5e1e3859406d5c916c58fbdc2ddeaddb1b8
Kaelin Noemi <noemi.kaelin@c-alm.ch>
2021-01-11 19:23:55 +0100

    added test for item&usercontroller (user not working because UserService not autowired in UserController)

---
+++ src/test/java/ch/zhaw/mas/sharingAppServer/ItemControllerTest.java
@@ -0,0 +1,49 @@
+package ch.zhaw.mas.sharingAppServer;
+
+import ch.zhaw.mas.sharingAppServer.serverSite.domain.ItemController;
+import ch.zhaw.mas.sharingAppServer.serverSite.domain.ItemModel;
+import ch.zhaw.mas.sharingAppServer.serverSite.domain.ItemService;
+import ch.zhaw.mas.sharingAppServer.serverSite.domain.UserInterface;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ItemController.class)
+@RunWith(SpringRunner.class)
+public class ItemControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ItemService itemService;
+
+    @Test
+    public void getAllItemsTest() throws Exception {
+        ItemModel item = new ItemModel();
+        item.setName("test item");
+        List<ItemModel> itemList = Arrays.asList(item);
+
+        given(itemService.getAllItems()).willReturn(itemList);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/items"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)));
+
+    }
+}
